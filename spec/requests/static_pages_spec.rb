6f5e3807bd2ff00fd733845a6c3b962cd4529fee
Luke Helminiak <lukehelminiak@gmail.com>
2016-07-13 17:58:50 -0500

    Initialize repository

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/'
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
